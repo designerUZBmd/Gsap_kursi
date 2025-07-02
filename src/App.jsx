838bb6f572af54839cd083473694aa577d9533d2
Oblaqulov <oblaqulov@Oblaqulovs-MacBook-Pro.local>
2025-07-02 12:57:09 +0500

    Implimet Hure animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Nawbar from "./Components/Nawbar.jsx";
+import Hero from "./Components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300">Hello Gsap</h1>
-        </div>
+        <main>
+            <Nawbar/>
+            <Hero/>
+        </main>
     )
 }
 export default App
